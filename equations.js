22dd7fba0adb0f91639cc5bb09eb14710dc87957
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-02-06 11:26:32 -0300

    feature: getting max height

--- equations.js
+++ equations.js
@@ -1,8 +1,17 @@
-//Implementar equação para descobrir a altura máxima
-function getMaxHeight() {
-    //equation
+const gravity = 9.81
 
-    return //result
+//Decomposição da velocidade inicial em velocidade horizontal (vx) e velocidade vertical inicial(v0y)
+function getVx(v0, angle) {
+    return v0 * Math.cos(DegreesToRadians(angle))
+}
+
+function getV0y(v0, angle) {
+    return v0 * Math.sin(DegreesToRadians(angle))
+}
+
+//Implementar equação para descobrir a altura máxima
+function getMaxHeight(v0y) {
+    return (v0y ** 2) / (2 * gravity)
 }
 
 //Implementar equação para descobrir o tempo de subida e de voo
@@ -18,3 +27,14 @@ function getDistance() {
 
     return //result
 }
+
+//Conversão entre radianos e graus
+function DegreesToRadians(degree) {
+    return degree * (Math.PI / 180)
+}
+
+function RadiansToDegrees(radian) {
+    return radian * (180 / Math.PI)
+}
+
+console.log(getMaxHeight(getV0y(18, 50)).toFixed(2))
