b37a32c31fda3412b249f2990294ef649861ab66
Vinicius Jimenez <vinnicius.jimenez@gmail.com>
2022-03-10 11:00:14 -0300

    finished: projectile motion launch

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,44 +13,94 @@ const resultLabels = {
     flightTime: document.querySelector('#flight-time')
 }
 
+const launchedProjectiles = []
+
 const canvasRenderer = CanvasRenderer.initialize()
 
-const showProjectileTrajectory = ({ x, y }) => canvasRenderer.draw(context => {
-    context.beginPath();
-    context.arc(x, y, 2.5, 0, Math.PI * 2);
-    context.closePath();
-    context.fill();
-})
+const showProjectileTrajectory = ({ x, y }, { style   }) => {
+    canvasRenderer.draw(context => {
+        context.fillStyle = style 
+        context.strokeStyle = style
+
+        context.beginPath();
+        context.arc(x, y, 2.5, 0, Math.PI * 2);
+        context.closePath();
+        context.fill();
+    })
+}
  
-const showProjectileParable = ({ sx, sy, ex, ey }) => canvasRenderer.draw(context => {
-    context.beginPath()
-    context.moveTo(sx, sy)
-    context.lineTo(ex, ey)
-    context.stroke()
-})
+const showProjectileParable = ({ sx, sy, ex, ey }, { style  }) => {
+    canvasRenderer.draw(context => {
+        context.fillStyle = style 
+        context.strokeStyle = style
+        
+        context.beginPath()
+        context.moveTo(sx, sy)
+        context.lineTo(ex, ey)
+        context.stroke()
+    })
+}
 
 const showTrajectory = (coords) => {
     coords.map(coord => {
         const { sx, sy, ex: x, ey: y } = coord
 
-        showProjectileParable({ sx, sy, ex: x, ey: y })
-        showProjectileTrajectory({ x, y })
+        showProjectileParable({ sx, sy, ex: x, ey: y }, { style: '#F78154' })
+        showProjectileTrajectory({ x, y }, { style: '#F78154' })
     })
 }
 
-const showProjectile = ({ x, y }) => {
-    const projectileImage = new Image()
+const showProjectile = ({ x, y }, { style }) => {
+    canvasRenderer.draw(context => {
+        context.fillStyle = style 
+        context.strokeStyle = style
 
-    projectileImage.src = 'images/projectile.png'
+        context.beginPath();
+        context.arc(x, y, 17, 0, Math.PI * 2);
+        context.closePath();
+        context.fill();
+    })
+}
 
-    projectileImage.addEventListener('load', event => {
-        x -= projectileImage.width / 2
-        y -= projectileImage.height / 2
+const showAllLaunchedProjectiles = () => {
+    launchedProjectiles.map(projectileCoords => {
+        projectileCoords.map(({ sx, sy, ex, ey }, index) => {
+            showProjectileParable({ sx, sy, ex, ey }, { style: '#000' })
+            showProjectileTrajectory({ x: ex, y: ey }, { style: '#000' })
 
-        canvasRenderer.draw(context => {
-            context.drawImage(projectileImage, x , y)
+            if(projectileCoords.length - 1 == index){
+                showProjectile({ x: ex, y: ey }, { style: '#424340' })
+            }
         })
     })
+}
+
+const showProjectileAtLastPosition = projectileMotion => {
+    const trajectoryCoords = [...JSON.parse(localStorage.getItem('current-trajectory-coords'))]
+    const lastCoord = trajectoryCoords.at(-1)
+    
+    const data = projectileMotion.getPositionAtTime(projectileMotion.getFlightTime(), { finalTime: true })
+    const rectCoords = { x: data.x * 15, y: ((canvasRenderer.height - 90) - (data.y * 15)) }
+
+    const currentTrajectory = {
+        sx: lastCoord.ex,
+        sy: lastCoord.ey,
+        ex: rectCoords.x,
+        ey: rectCoords.y
+    }
+
+    canvasRenderer.clear({ x: (currentTrajectory.sx - 17), y: (currentTrajectory.sy - 17), width: 34, height: 34 })
+
+    trajectoryCoords.push(currentTrajectory)
+
+    if(!!launchedProjectiles.length){
+        showAllLaunchedProjectiles()
+    }
+
+    showTrajectory(trajectoryCoords)
+    showProjectile(rectCoords, { style: '#F78154' }) 
+
+    localStorage.setItem('current-trajectory-coords', JSON.stringify(trajectoryCoords))
 }
 
 const createProjectileTrajectory = (projectileMotion, time = 0) => {
@@ -73,8 +123,14 @@ const createProjectileTrajectory = (projectileMotion, time = 0) => {
 
         canvasRenderer.reset()
 
-        showProjectile(rectCoords)
+        if(!!launchedProjectiles.length){
+            showAllLaunchedProjectiles()
+        }
+
         showTrajectory(trajectoryCoords)
+        showProjectile(rectCoords, { style: '#F78154' }) 
+
+        localStorage.setItem('current-trajectory-coords', JSON.stringify(trajectoryCoords))
 
         time++
     }, 100)
@@ -103,8 +159,16 @@ startButton.addEventListener('click', event => {
     setTimeout(() => {
         startButton.disabled = false
 
+        showProjectileAtLastPosition(projectileMotion)
+
+        launchedProjectiles.push(JSON.parse(localStorage.getItem('current-trajectory-coords')))
+
         clearInterval(interval)
     }, projectileMotion.getFlightTime() * 1000)
 })
 
-cleanButton.addEventListener('click', () => canvasRenderer.reset())
+cleanButton.addEventListener('click', () => {
+    launchedProjectiles.length = 0
+
+    canvasRenderer.reset()
+})
